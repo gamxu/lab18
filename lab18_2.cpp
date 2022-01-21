4e125a7cd0dc0cb727e8d7e0c88b470bd393b9d5
gamxu <wuttipat.did@gmail.com>
2022-01-21 18:43:01 +0700

    lab18_2 completed

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 using namespace std;
 
-________ myString(_____________________){
-	_______ = new ____________;    
-	for(int i = 0; i < N;i++) ____________ = 'A'+i;
-	__________ = 0;
+void myString(char *(&q), int N){
+	q = new char[N];    
+	for(int i = 0; i < N;i++) q[i] = 'A'+ i;
+	q[N] = 0;
 }
 
 int main(){
